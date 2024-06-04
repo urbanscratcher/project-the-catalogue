42bbbb4b9b2ca17aee3afe293e316ebc394840bf
Joun <urbanscratcher@gmail.com>
2024-06-05 01:19:22 +0900

    scrolltotop fixed

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -37,8 +37,14 @@ function Page({ params }: { params: { slug: string } }) {
   }, [url]);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [text]);
+    if (
+      isSlugProjects ||
+      slug.startsWith("analysis-experience") ||
+      slug.startsWith("backend-experience")
+    ) {
+      window.scrollTo(0, 0);
+    }
+  }, [slug, isSlugProjects]);
 
   return (
     <section className="prose max-w-3xl mx-auto prose-code:before:content-none prose-code:after:content-none prose-h1:text-7xl prose-h1:break-words prose-h2:text-3xl prose-h1:text-center prose-h1:font-normal prose-h1:leading-tight prose-h1:tracking-wide prose-code:bg-gray-200 prose-code:px-[6px] prose-code:py-[2px] prose-code:rounded-[4px] prose-h2:tracking-wide prose-code:text-base">
