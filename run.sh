96afec1ce2d1e576b139119f77f9a430eab9a5a2
Joun <urbanscratcher@gmail.com>
2024-06-13 01:03:52 +0900

    .

--- run.sh
+++ run.sh
@@ -2,3 +2,9 @@
 
 # Docker Compose를 사용하여 빌드 및 실행
 docker-compose up --build
+
+# 첫 번째 명령어가 실패한 경우
+if [ $? -ne 0 ]; then
+  echo "docker-compose up --build failed. Trying docker compose up --build..."
+  docker compose up --build
+fi
